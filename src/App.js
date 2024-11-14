9800e4df00c0809f18b0b4e680195eaf6ffc7ece
Bryan Hong <bzh.texas@gmail.com>
2024-11-13 18:22:03 -0800

    fixed words

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           <img src = "IMG_1149 (1).jpg" alt = "Bryan Hong" width="400" height="400"/>
           <div className="heading"><h1><TypingText/></h1><p>I'm a student at  
             <span style={{color: 'blue'}}><b> UC Santa Barbara </b> </span> studying
-            <span style={{color: 'blue'}}><b> CS + Statistics & Data Science </b></span> with a minor in <span style={{color: 'blue'}}><b> Pure Math. </b></span> My project experience spans diverse fields—including urban planning, bioinformatics, public health, and more—but at the core, each is united by my passion for <b>machine and deep learning, computer vision, & predictive modeling.</b> I value curiosity, creativity, and a commitment to facts and originality, and I'm a problem-solver at heart. Explore some of my personal and research projects below!</p>
+            <span style={{color: 'blue'}}><b> CS + Statistics & Data Science </b></span> with a minor in <span style={{color: 'blue'}}><b> Pure Math. </b></span> My project experience spans diverse fields—including urban planning, bioinformatics, public health, and more—but at the core, each is united by my passion for <b>Machine and Deep Learning, Computer Vision, & Predictive Modeling.</b> I value curiosity, creativity, and a commitment to facts and originality, and I'm a problem-solver at heart. Explore some of my personal and research projects below!</p>
             <p><b>CURRENTLY BUILDING: <span style={{color: 'Purple'}}>Alexandria - your AI reading assistant.</span></b></p></div>
 
           </div>
